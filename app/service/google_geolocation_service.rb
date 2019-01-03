57d93a88d52ac79808b7da3df1f85f70171fdf17
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-01-02 20:54:27 -0700

    clean up notes for current location

--- app/service/google_geolocation_service.rb
+++ app/service/google_geolocation_service.rb
@@ -15,20 +15,3 @@ class GoogleGeolocationService
     JSON.parse(conn.get(url).body, symbolize_names: true)[:results]
   end
 end
-
-  # def get_geolocation_coordinates
-  #   get_json("/geolocation/v1/geolocate?key=#{ENV['GOOGLE_API_KEY']}")[:location]
-  # end
-  #
-  # private
-  #
-  # def conn
-  #   Faraday.new(url:"https://www.googleapis.com") do |faraday|
-  #     faraday.params["key"] = ENV['GOOGLE_API_KEY']
-  #     faraday.adapter Faraday.default_adapter
-  #   end
-  # end
-  #
-  # def get_json(url)
-  #   JSON.parse(conn.post(url).body, symbolize_names: true)
-  # end
--- spec/services/google_geolocation_spec.rb
+++ spec/services/google_geolocation_spec.rb
@@ -14,23 +14,3 @@ describe GoogleGeolocationService do
     end
   end
 end
-
-
-
-# require 'rails_helper'
-#
-# describe GoogleGeolocationService do
-#   it 'exists' do
-#     VCR.use_cassette("google_geolocation_service") do
-#     service = GoogleGeolocationService.new
-#     expect(service).to be_a(GoogleGeolocationService)
-#     end
-#   end
-#   it 'returns coordinates' do
-#     VCR.use_cassette("google_geolocation_service") do
-#       service = GoogleGeolocationService.new
-#       expect(service.get_geolocation_coordinates[:lat].class).to be(Float)
-#       expect(service.get_geolocation_coordinates[:lng].class).to be(Float)
-#     end
-#   end
-# end
