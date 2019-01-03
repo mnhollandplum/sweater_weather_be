24a9327476d77911fd283b277e74311782c242de
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-01-02 18:38:45 -0700

    update gemfile, write first test. create first controller and update route

---
+++ spec/requests/api/v1/geolocation_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "API request" do
+
+it 'API request is successful' do
+
+    VCR.use_cassette("can send forecast request") do
+      get "/api/v1/forecast?location=denver,co"
+    end
+
+    expect(response).to be_successful
+
+    forecast = JSON.parse(response.body)
+  end
+end
