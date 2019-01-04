58fdc7f9a76c4579ca6ede28775c255a311e9f36
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-01-03 19:35:16 -0700

    successfully creates a user and displays json via api endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    user.save
+    render json: {api_key: user.api_key}, status: 201
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
--- spec/requests/api/v1/user_creation_request_spec.rb
+++ spec/requests/api/v1/user_creation_request_spec.rb
@@ -8,6 +8,10 @@ describe 'User Account Creation' do
                                     password: "password",
                                     password_confirmation: "password"
                                   }
+    user_json= JSON.parse(response.body, symbolize_names: true)
+
     expect(response.status).to eq(201)
+    expect(user_json).to have_key(:api_key)
+
   end
 end
