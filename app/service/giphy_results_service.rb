32027c2ba2bb77d52c33b2301ff2981e82e3816b
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-01-04 09:04:45 -0700

    write service test and retrieve json response

---
+++ app/service/giphy_results_service.rb
@@ -0,0 +1,19 @@
+class GiphyResultsService
+
+  def get_gifs(search)
+    get_json("/v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}&q=#{search}")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url:"https://api.giphy.com") do |faraday|
+      faraday.params["key"] = ENV["GIPHY_API_KEY"]
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
+end
---
+++ spec/services/giphy_service_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe GiphyResultsService do
+
+  it 'exists' do
+    VCR.use_cassette("giphy service") do
+      service = GiphyResultsService.new
+      expect(service).to be_a(GiphyResultsService)
+    end
+  end
+
+  it 'returns gif data' do
+    VCR.use_cassette("giphyservice") do
+      service = GiphyResultsService.new
+
+      expect(service.get_gifs("Cheeseburger")).to be_an_instance_of(Hash)
+      expect(service.get_gifs("Cheeseburger").keys).to eq([:data, :pagination, :meta])
+      expect(service.get_gifs("Cheeseburger")[:data][0][:url]).to eq("https://giphy.com/gifs/friends-los-angeles-burger-XXsyB8OWePXva")
+
+   end
+ end
+end
