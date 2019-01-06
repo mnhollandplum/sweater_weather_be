4d8010beaddaa7bfc7ff84d7e1349409bae0ff25
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-01-06 14:14:10 -0700

    fixing vcr

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#index'
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
+      get '/gifs', to: 'gifs#index'
     end
   end
 end
